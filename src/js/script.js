bf04dd50101c252865e975c24cd3b95932576e7b
Ilya <olovyannikov_barista@icloud.com>
2021-04-28 10:22:26 +0300

    Добавил скролл меню

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,16 @@ window.addEventListener('DOMContentLoaded', () => {
     const $menuBtn = document.querySelector('#menuBtn');
     const $offCanvasNav = document.querySelector('.nav__list');
     const $overlay = document.querySelector('.overlay');
+    const $header = document.querySelector('.js-header');
     const _submenuBtns = document.querySelectorAll('.nav__link_hasSubMenu');
+    let scrollPrev = 0;
 
-    _submenuBtns.forEach(button => {
-        button.addEventListener('click', (e) => {
-            button.classList.toggle('nav__link_active');
-        });
+    window.addEventListener('scroll', () => {
+        let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        scrolled > 100 && scrolled > scrollPrev ?
+            $header.classList.add('out') : $header.classList.remove('out');
+        scrollPrev = scrolled;
     });
-
 
     const toggleMenu = (e) => {
         $menuBtn.classList.toggle('header__menu-button_active');
@@ -19,9 +21,15 @@ window.addEventListener('DOMContentLoaded', () => {
         document.body.addEventListener('click', e => {
             if (e.target.classList.contains('overlay')) {
                 toggleMenu()
-            };
+            }
         });
     };
+
+    _submenuBtns.forEach(button => {
+        button.addEventListener('click', () => {
+            button.classList.toggle('nav__link_active');
+        });
+    });
 
     $menuBtn.addEventListener('click', () => toggleMenu());
 });
