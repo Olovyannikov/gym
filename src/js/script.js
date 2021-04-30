4a18ee320636645f2d407bc9196a07e72b47550a
Илья Оловянников <ilaolovannikov@iMac-Ila.local>
2021-04-30 14:53:08 +0300

    Добавил анимацию waves в scaffoldings

--- src/js/script.js
+++ src/js/script.js
@@ -8,10 +8,12 @@ const init = () => {
     window.onload = function () {
         const preloader = document.querySelector('.preloader');
         preloader.classList.add('preloader-animation');
+        document.body.classList.add('overflow');
 
         setTimeout(() => {
             preloader.classList.remove('preloader-animation');
             preloader.classList.add('preloader-hidden');
+            document.body.classList.remove('overflow');
         }, 3000);
         setTimeout(() => {
             startAnimation();
