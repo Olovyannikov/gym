46fcd0146d8a1ffd09a55baf1ece13b415d21b32
Ilya <olovyannikov_barista@icloud.com>
2021-05-12 15:44:36 +0300

    Страница Бег

--- src/js/modules/menu-toggle.js
+++ src/js/modules/menu-toggle.js
@@ -1,17 +1,35 @@
 export default () => {
-    const $menuBtn = document.querySelector('#menuBtn');
-    const $offCanvasNav = document.querySelector('.nav__list');
-    const $overlay = document.querySelector('.overlay');
-    function toggleMenu() {
-        $menuBtn.classList.toggle('header__menu-button_active');
-        $offCanvasNav.classList.toggle('nav__list_active');
-        $overlay.classList.toggle('active');
-        document.body.classList.toggle('menu_open');
-        document.body.addEventListener('click', e => {
-            if (e.target.classList.contains('overlay')) {
-                toggleMenu();
-            }
-        });
+  const $menuBtn = document.querySelector('#menuBtn');
+  const $offCanvasNav = document.querySelector('.nav__list');
+  const $overlay = document.querySelector('.overlay');
+  const _submenuBtns = document.querySelectorAll('.nav__link_hasSubMenu');
+
+  function toggleMenu() {
+    $menuBtn.classList.toggle('header__menu-button_active');
+    $offCanvasNav.classList.toggle('nav__list_active');
+    $overlay.classList.toggle('active');
+    document.body.classList.toggle('menu_open');
+  }
+
+  _submenuBtns.forEach(button => {
+    button.addEventListener('click', (e) => {
+      $menuBtn.classList.add('header__menu-button_active');
+      $offCanvasNav.classList.add('nav__list_active');
+      $overlay.classList.add('active');
+      document.body.classList.add('menu_open');
+      button.classList.toggle('nav__link_active');
+    });
+  });
+
+  $overlay.addEventListener('click', e => {
+    if(e.target.classList.contains('active')) {
+      _submenuBtns.forEach(btn => btn.classList.remove('nav__link_active'))
+      $menuBtn.classList.remove('header__menu-button_active');
+      $offCanvasNav.classList.remove('nav__list_active');
+      $overlay.classList.remove('active')
+      document.body.classList.remove('menu_open');
     }
-    $menuBtn.addEventListener('click', () => toggleMenu());
+  })
+
+  $menuBtn.addEventListener('click', () => toggleMenu());
 }
--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,28 @@
 //import modules
 import gsap from "gsap";
 import bezier from "./vendor/bezier";
+
 bezier();
 import startAnimation from "./modules/start-animation";
 import {default as toggleMenu} from "./modules/menu-toggle";
 
 const init = () => {
-    window.onload = function () {
-        const preloader = document.querySelector('.preloader');
-        preloader.classList.add('preloader-animation');
-        document.body.classList.add('overflow');
+  window.onload = function () {
+    const preloader = document.querySelector('.preloader');
+    preloader.classList.add('preloader-animation');
+    document.body.classList.add('overflow');
 
-        setTimeout(() => {
-            preloader.classList.remove('preloader-animation');
-            preloader.classList.add('preloader-hidden');
-            document.body.classList.remove('overflow');
-        }, 3000);
-        setTimeout(() => {
-            startAnimation();
-            toggleMenu();
-            preloader.classList.add('preloader-none');
-        }, 3200);
-    }
+    setTimeout(() => {
+      preloader.classList.remove('preloader-animation');
+      preloader.classList.add('preloader-hidden');
+      document.body.classList.remove('overflow');
+    }, 3000);
+    setTimeout(() => {
+      startAnimation();
+      toggleMenu();
+      preloader.classList.add('preloader-none');
+    }, 3200);
+  }
 
 
 }
